659f07767e4a33148891cd4e247cf55e98ce5b8d
Mikwuegbu <mstephen130@yahoo.com>
2024-10-16 12:20:00 +0100

    npm updated

--- 01-node-tutorial/08-os-module.js
+++ 01-node-tutorial/08-os-module.js
@@ -1,16 +1,19 @@
-const os = require('os')
+const os = require('os');
 
 // info about current user
-const user = os.userInfo()
-console.log(user)
+const user = os.userInfo();
+console.log(user);
 
 // method returns the system uptime in seconds
-console.log(`The System Uptime is ${os.uptime()} seconds`)
+console.log(`The System Uptime is ${os.uptime()} seconds`);
 
+// other methods in os module
 const currentOS = {
-  name: os.type(),
-  release: os.release(),
-  totalMem: os.totalmem(),
-  freeMem: os.freemem(),
-}
-console.log(currentOS)
+	name: os.type(),
+	release: os.release(),
+	totalMem: os.totalmem(),
+	freeMem: os.freemem(),
+	platform: os.platform(),
+	machine: os.machine(),
+};
+console.log(currentOS);
