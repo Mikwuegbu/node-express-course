e4404d8a8bd65c2b4d9543db6e9beef6e920a3b7
Mikwuegbu <mstephen130@yahoo.com>
2025-01-16 10:13:04 +0100

    resume learning

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,41 +1,16 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-
-// const PORT = 5000;
-
-// app.use(express.static('./navbar-app'));
-// app.get('/', (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// });
-
-// app.all('*', (req, res) => {
-// 	res.status(404).send('Resource not found');
-// });
-
-// app.listen(PORT, () => {
-// 	console.log(`listening on ${PORT}`);
-// });
-
 const express = require('express');
 const app = express();
-const { products, people } = require('./data');
+
+// middleware
+
 app.get('/', (req, res) => {
-	res.send('<h1>Hompage</h1><a href="/api/products">products</a>');
+	res.send('Hello from Express!');
 });
 
-app.get('/api/products/:productID', (req, res) => {
-	const { productID } = req.params;
-	const singleProduct = products.find(
-		(product) => product.id === Number(productID)
-	);
-
-	if (!singleProduct) {
-		return res.status(404).send('Product not found');
-	}
-
-	return res.json(singleProduct);
+app.get('/about', (req, res) => {
+	res.send('Hello from About!');
 });
+
 app.listen(5000, () => {
 	console.log('Server is listening on port 5000....');
 });
